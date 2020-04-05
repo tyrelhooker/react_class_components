a29d6a15203b7afbdc10c0feb394c63bfe23010a
TJ Hooker <tyrelhooker@gmail.com>
2020-04-05 13:51:23 -0400

    Create react app to test async, fetch data, and react functional components vs. React classes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+  // const myGreetings = [
+  //   {
+  //     normal: 'Hello Function Component', 
+  //     happy: 'HELLO!!!!! Function Component',
+  //     sad: '...hellooo, function compooonent'
+  //   }
+  // ];
+
+  const getDummyData = (count = 5) => {
+    fetch(`https://api.randomuser.me/?nat=US&results=${count}`)
+    .then(response => response.json())
+    .then(data => {
+      const myData = data;
+      console.log(myData);
+      // return myData
+      return (
+
+       
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {myData}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      )
+    })
+    .catch(err => console.error(`There has been an error with your fetch operation ${err.message}`))
+  }
+  
+  console.log(getDummyData(10));
+
+  return (getDummyData(10))
+
 }
+  
+// const Headline = ({ dummyData }) => {
+//   console.log({dummyData})
+//   return (
+//     dummyData.map(items => 
+//       <p>{items.results}</p>
+//     )
+//   )
+// }
+
+ 
 
 export default App;
